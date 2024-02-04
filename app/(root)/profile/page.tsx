ba1c799ce86b937c9358347fdc0c173f158d6fd0
Sahil Darunde <Sahildarunde007@gmail.com>
2024-02-05 02:39:08 +0530

    deployment

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,107 +1,78 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
-import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
-import { clerkClient } from '@clerk/nextjs'
-import { NextResponse } from 'next/server'
- 
-export async function POST(req: Request) {
- 
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
- 
-  if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
-  }
- 
-  // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
- 
-  // If there are no headers, error out
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
-      status: 400
-    })
-  }
- 
-  // Get the body
-  const payload = await req.json()
-  const body = JSON.stringify(payload);
- 
-  // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
- 
-  let evt: WebhookEvent
- 
-  // Verify the payload with the headers
-  try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent
-  } catch (err) {
-    console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
-      status: 400
-    })
-  }
- 
-  // Get the ID and type
-  const { id } = evt.data;
-  const eventType = evt.type;
- 
-  if(eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+import Collection from '@/components/shared/Collection'
+import { Button } from '@/components/ui/button'
+import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
+import { auth } from '@clerk/nextjs'
+import Link from 'next/link'
+import React from 'react'
 
-    const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    }
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
 
-    const newUser = await createUser(user);
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-    if(newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id
-        }
-      })
-    }
+  const orders = await getOrdersByUser({ userId, page: ordersPage})
 
-    return NextResponse.json({ message: 'OK', user: newUser })
-  }
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
-  if (eventType === 'user.updated') {
-    const {id, image_url, first_name, last_name, username } = evt.data
+  return (
+    <>
+      {/* My Tickets */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className='h3-bold text-center sm:text-left'>My Tickets</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/#events">
+              Explore More Events
+            </Link>
+          </Button>
+        </div>
+      </section>
 
-    const user = {
-      firstName: first_name,
-      lastName: last_name,
-      username: username!,
-      photo: image_url,
-    }
+      <section className="wrapper my-8">
+        <Collection 
+          data={orderedEvents}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubtext="No worries - plenty of exciting events to explore!"
+          collectionType="My_Tickets"
+          limit={3}
+          page={ordersPage}
+          urlParamName="ordersPage"
+          totalPages={orders?.totalPages}
+        />
+      </section>
 
-    const updatedUser = await updateUser(id, user)
+      {/* Events Organized */}
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <div className="wrapper flex items-center justify-center sm:justify-between">
+          <h3 className='h3-bold text-center sm:text-left'>Events Organized</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/events/create">
+              Create New Event
+            </Link>
+          </Button>
+        </div>
+      </section>
 
-    return NextResponse.json({ message: 'OK', user: updatedUser })
-  }
-
-  if (eventType === 'user.deleted') {
-    const { id } = evt.data
-
-    const deletedUser = await deleteUser(id!)
-
-    return NextResponse.json({ message: 'OK', user: deletedUser })
-  }
- 
-  return new Response('', { status: 200 })
+      <section className="wrapper my-8">
+        <Collection 
+          data={organizedEvents?.data}
+          emptyTitle="No events have been created yet"
+          emptyStateSubtext="Go create some now"
+          collectionType="Events_Organized"
+          limit={3}
+          page={eventsPage}
+          urlParamName="eventsPage"
+          totalPages={organizedEvents?.totalPages}
+        />
+      </section>
+    </>
+  )
 }
- 
+
+export default ProfilePage
