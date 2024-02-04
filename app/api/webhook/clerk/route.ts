1553d4f762217c66fcb66da2a4ff15c392731722
Sahil Darunde <Sahildarunde007@gmail.com>
2024-02-05 01:33:56 +0530

    mongoose

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -8,7 +8,7 @@ import { NextResponse } from 'next/server'
 export async function POST(req: Request) {
  
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
  
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
-      }
+    }
 
     const newUser = await createUser(user);
 
